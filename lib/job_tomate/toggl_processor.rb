4a6ce0d014e9d80b6c59a31f9a600629bcaf41da
Romain Champourlier <pro@rchampourlier.com>
2015-05-22 00:18:10 +0200

    Minor refactoring of TogglProcessor + handling minimum oldness of time report of 2 hours before sending to JIRA

--- lib/job_tomate/toggl_processor.rb
+++ lib/job_tomate/toggl_processor.rb
@@ -12,21 +12,22 @@ module JobTomate
     def self.run
       reports = TogglClient.fetch_reports(Date.yesterday, Date.today)
       reports.map do |toggl_report|
-        if jira_report?(toggl_report)
-          entry = create_or_update_entry(toggl_report)
-          if add_to_jira?(entry)
-            if add_worklog_to_jira(toggl_report)
-              mark_entry_added_to_jira(entry)
-              :sent_to_jira
-            else
-              :failed_to_send_to_jira
-            end
-          else
-            :already_sent_to_jira
-          end
-        else
-          :ignored
-        end
+        result = process_toggl_report(toggl_report)
+      end
+    end
+
+    def self.process_toggl_report(toggl_report)
+      return :not_linked_to_jira unless linkable_to_jira?(toggl_report)
+      return :not_old_enough unless old_enough?(toggl_report)
+
+      entry = create_or_update_entry(toggl_report)
+      return :already_sent_to_jira if sent_to_jira?(entry)
+
+      if add_worklog_to_jira(toggl_report)
+        mark_entry_added_to_jira(entry)
+        :successfully_sent_to_jira
+      else
+        :failed_to_send_to_jira
       end
     end
 
@@ -41,7 +42,7 @@ module JobTomate
       else
         entry = TogglEntry.new
         entry.toggl_id = toggl_id
-        entry.status = 'new'
+        entry.status = 'pending'
       end
 
       entry.toggl_updated = toggl_updated
@@ -49,8 +50,14 @@ module JobTomate
       entry
     end
 
-    def self.jira_report?(toggl_report)
+    # Returns `true` if the Toggl report can be linked to a JIRA issue.
+    def self.linkable_to_jira?(toggl_report)
       !!(jira_issue_key(toggl_report))
+    end
+
+    def self.old_enough?(toggl_report)
+      updated_at = Time.parse(toggl_report['updated'])
+      updated_at < 2.hours.ago
     end
 
     def self.jira_issue_key(toggl_report)
@@ -61,9 +68,11 @@ module JobTomate
       toggl_report['dur'] / 1000
     end
 
+    # Returns `true` if the Toggl entry has been sent to JIRA,
+    # i.e. the status contains `sent_to_jira`.
     # @param entry [TogglEntry]
-    def self.add_to_jira?(entry)
-      !!(entry.status =~ /new/) # accepts "new" and "new_updated"
+    def self.sent_to_jira?(entry)
+      !!(entry.status =~ /sent_to_jira/)
     end
 
     def self.credentials_for_toggl_report(toggl_report)
