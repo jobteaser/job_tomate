a0a3646330ced6efc32078adfe9538bc609487f1
Romain Champourlier <pro@rchampourlier.com>
2015-08-06 12:06:21 +0200

    Fixed console script (moved to /bin)

--- lib/job_tomate/all.rb
+++
@@ -1,6 +0,0 @@
-# require 'job_tomate/toggl_client'
-# require 'job_tomate/toggl_entry'
-# require 'job_tomate/toggl_processor'
-# require 'job_tomate/jira_client'
-# require 'job_tomate/user'
-# require 'job_tomate/loginfo'
