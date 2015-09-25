29d597a5048ac8b1c262e02bc490deeacd79e44c
Romain Champourlier <pro@rchampourlier.com>
2015-09-25 12:44:12 +0200

    Refactored GithubProcessor

--- lib/job_tomate/github_processor.rb
+++ lib/job_tomate/github_processor.rb
@@ -1,8 +1,9 @@
 require 'active_support/all'
-require 'job_tomate/user'
 require 'job_tomate/interface/jira_client'
 
 module JobTomate
+
+  # Processor for Github events triggered through webhook (see `webhooks_handler.rb`).
   class GithubProcessor
 
     # Perform a run where:
@@ -10,21 +11,51 @@ module JobTomate
     #   - defines params from the webhook in order to make a POST request on Jira,
     #   - set the correct comment considering the PR status.
     def self.run(webhook_data)
-      branche = webhook_data['pull_request']['head']['ref']
-      issue_key = branche[/jt-[\d]+/i]
-      github_user = webhook_data['pull_request']['user']['login']
-      user = User.where(github_user: github_user).first
-      user ? [user.jira_username, user.jira_password] : nil
-      if webhook_data['action'] == 'closed'
-        comment = if webhook_data['pull_request']['merged']
-                    "Merged PR in #{webhook_data['pull_request']['base']['ref']} (via job_tomate)"
-                  else
-                    "PR closed but not merged (via job_tomate)"
-                  end
-      elsif webhook_data['action'] == 'opened'
-        comment = "Opened PR : #{webhook_data['pull_request']['html_url']} (via job_tomate)"
+      add_jira_comment_on_pr_event(webhook_data)
+    end
+
+    # IMPLEMENTATION
+
+    def self.add_jira_comment_on_pr_event(webhook_data)
+      comment = jira_comment_on_pr_event(webhook_data)
+      return if comment.nil?
+      JobTomate::Interface::JiraClient.add_comment(
+        issue_key(webhook_data),
+        ENV['JIRA_USERNAME'], ENV['JIRA_PASSWORD'],
+        comment)
+    end
+
+    def self.jira_comment_on_pr_event(webhook_data)
+      if pr_action(webhook_data) == 'closed'
+        if pr_merged?(webhook_data)
+          return "Merged PR in #{pr_base_ref(webhook_data)}"
+        end
+        return 'PR closed but not merged'
       end
-      JobTomate::Interface::JiraClient.add_comment(issue_key, user.jira_username, user.jira_password, comment)
+      if pr_action(webhook_data) == 'opened'
+        return "Opened PR: #{pr_html_url(webhook_data)}"
+      end
+    end
+
+    def self.issue_key(webhook_data)
+      branch = webhook_data['pull_request']['head']['ref']
+      branch[/jt-[\d]+/i]
+    end
+
+    def self.pr_action(webhook_data)
+      webhook_data['action']
+    end
+
+    def self.pr_merged?(webhook_data)
+      webhook_data['pull_request']['merged'].present?
+    end
+
+    def self.pr_base_ref(webhook_data)
+      webhook_data['pull_request']['base']['ref']
+    end
+
+    def self.pr_html_url(webhook_data)
+      webhook_data['pull_request']['html_url']
     end
   end
 end
