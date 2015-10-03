15512ff9ad41989b844f2ae977fec77f2d1c1530
Romain Champourlier <pro@rchampourlier.com>
2015-10-03 16:23:51 +0200

    Refactored Input::Jira::Processor
    Handling dynamically ...Rules modules

--- lib/job_tomate/input/jira/processor.rb
+++ lib/job_tomate/input/jira/processor.rb
@@ -1,6 +1,5 @@
 require 'active_support/all'
-require 'job_tomate/input/jira/status_rules'
-require 'job_tomate/input/jira/alerting_rules'
+Dir[File.expand_path('../*_rules.rb', __FILE__)].each { |f| require f }
 
 module JobTomate
   module Input
@@ -19,8 +18,27 @@ module JobTomate
         # the issue change. If the user is not available on JobTomate, the
         # first user in the database is used.
         def self.run(webhook_data)
-          StatusRules.apply(webhook_data)
-          AlertingRules.apply(webhook_data)
+          rules_modules.each { |rules_module| apply(rules_module, webhook_data) }
+        end
+
+        # Applying the specified rules.
+        # Exceptions are caught to prevent breaking the application
+        # of other rules coming after.
+        def self.apply(rules, webhook_data)
+          begin
+            rules.apply(webhook_data)
+          rescue => e
+            LOGGER.error e
+          end
+        end
+
+        def self.rules_modules
+          rules_module_constants = JobTomate::Input::Jira.constants.select do |rules_module|
+            rules_module.to_s =~ /Rules\Z/
+          end
+          rules_module_constants.map do |rules_module|
+            JobTomate::Input::Jira.const_get(rules_module)
+          end
         end
       end
     end
