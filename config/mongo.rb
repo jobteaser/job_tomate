dabd91edae8b1216719ba1a7bc7c461c0f557511
Romain Champourlier <pro@rchampourlier.com>
2015-10-03 16:20:49 +0200

    Reduced Mongo logs

--- config/mongo.rb
+++ config/mongo.rb
@@ -1,9 +1,8 @@
-require 'config/boot'
 require 'mongoid'
 
 env = ENV['APP_ENV']
 raise 'APP_ENV environment variable must be set' if env.nil?
 
+Mongo::Logger.logger.level = ::Logger::INFO
 ENV['MONGOID_ENV'] = env
-
 Mongoid.load! File.expand_path('../mongoid.yml', __FILE__)
