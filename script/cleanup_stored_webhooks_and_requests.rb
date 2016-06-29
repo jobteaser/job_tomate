06d843623d94c3440bf055bbbe80f2b5093db68a
Romain Champourlier <pro@rchampourlier.com>
2016-06-29 22:12:26 +0200

    [hotfix] Fix dumb bug in cleanup script

--- script/cleanup_stored_webhooks_and_requests.rb
+++ script/cleanup_stored_webhooks_and_requests.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 
-# Cleanup webhook payloads older than 1 week
+# Cleanup:
+#   - requests older than 1 day,
+#   - webhooks older than 1 week.
+
 require_relative "../config/boot"
+require "job_tomate/data/stored_request"
 require "job_tomate/data/stored_webhook"
 
-JobTomate::Data::StoredWebhook.where(created_at: { :$gt => 1.week.ago }).destroy_all
-JobTomate::Data::StoredRequest.where(created_at: { :$gt => 1.day.ago }).destroy_all
+JobTomate::Data::StoredRequest.where(created_at: { :$lt => 1.day.ago }).destroy_all
+JobTomate::Data::StoredWebhook.where(created_at: { :$lt => 1.week.ago }).destroy_all
