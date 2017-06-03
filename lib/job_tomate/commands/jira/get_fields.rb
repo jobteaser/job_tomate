81a7b13cad301c8f3a324cd1e2df5143b43e6089
Romain Champourlier <pro@rchampourlier.com>
2017-06-03 18:19:21 +0200

    [issue#63] codeclimate fix

--- lib/job_tomate/commands/jira/get_fields.rb
+++ lib/job_tomate/commands/jira/get_fields.rb
@@ -7,6 +7,16 @@ module JobTomate
   module Commands
     module JIRA
 
+      # Command to fetch issue fields from JIRA. This is useful
+      # to find the identifier to use in code for a given custom
+      # field.
+      #
+      # How to use?
+      #
+      #    ENV["JIRA_USERNAME"] = <some jira username>
+      #    ENV["JIRA_PASSWORD"] = <the user's password>
+      #    JSON.parse(JobTomate::Commands::JIRA::GetFields.run().body)
+      #
       class GetFields
         extend ServicePattern
 
