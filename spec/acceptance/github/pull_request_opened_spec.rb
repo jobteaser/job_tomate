110c5ed13950595c3686d5a95c326dde2ca2f86c
Giniouxe <aurelie.ginioux@jobteaser.com>
2018-01-19 16:07:04 +0100

    Handle Career Services opend, closed and merged PR when commenting on the JIRA issue

--- spec/acceptance/github/pull_request_opened_spec.rb
+++ spec/acceptance/github/pull_request_opened_spec.rb
@@ -10,34 +10,86 @@ describe "/webhooks/github" do
     it "adds a comment on the JIRA with the PR link" do
       expected_comment = "Opened PR: https://github.com/jobteaser/job_tomate/pull/3 - branch: jt-1234-create-crawler"
       expected_body = "{\"body\":\"#{expected_comment}\"}"
-      stub = stub_jira_request(
-        :post,
-        "/issue/jt-1234/comment",
-        expected_body
-      )
-    stub_fill_branch_JIRA = stub_request(:put, "https://example.atlassian.net/rest/api/2/issue/jt-1234?startAt=0").
-         with(body: "{\"fields\":{\"Branch Name\":\"jt-1234-create-crawler\"}}",
-              headers: {'Authorization'=>'Basic am9iX3RvbWF0ZV91c2VybmFtZTpqb2JfdG9tYXRlX3B3ZA==', 'Content-Type'=>'application/json'}).
-         to_return(status: 200, body: "", headers: {})
-    receive_stored_webhook(:github_pull_request_opened_jira_related)
-    expect(stub).to have_been_requested
-    expect(stub_fill_branch_JIRA).to have_been_requested
+
+      post_comment = stub_jira_request(:post, "/issue/jt-1234/comment", expected_body)
+      fill_jira_branch = stub_request(
+        :put,
+        "https://example.atlassian.net/rest/api/2/issue/jt-1234?startAt=0"
+        ).with(
+          body: "{\"fields\":{\"Branch Name\":\"jt-1234-create-crawler\"}}",
+          headers: {
+            'Authorization'=>'Basic am9iX3RvbWF0ZV91c2VybmFtZTpqb2JfdG9tYXRlX3B3ZA==',
+            'Content-Type'=>'application/json'
+          }
+        ).to_return(status: 200, body: "", headers: {})
+
+      receive_stored_webhook(:github_pull_request_opened_jira_jobteaser_related)
+
+      expect(post_comment).to have_been_requested
+      expect(fill_jira_branch).to have_been_requested
     end
 
     it "sets the branch name field with the branch name" do
-      expected_body_2 = "{\"fields\":{\"Branch Name\":\"jt-1234-create-crawler\"}}"
-      stub_2 = stub_jira_request(
-        :put,
-        "/issue/jt-1234",
-        expected_body_2
-      )
-      stub_comment_JIRA = stub_request(:post, "https://example.atlassian.net/rest/api/2/issue/jt-1234/comment?startAt=0").
-         with(body: "{\"body\":\"Opened PR: https://github.com/jobteaser/job_tomate/pull/3 - branch: jt-1234-create-crawler\"}",
-              headers: {'Authorization'=>'Basic am9iX3RvbWF0ZV91c2VybmFtZTpqb2JfdG9tYXRlX3B3ZA==', 'Content-Type'=>'application/json'}).
-      to_return(status: 200, body: "", headers: {})
-      receive_stored_webhook(:github_pull_request_opened_jira_related)
-      expect(stub_2).to have_been_requested
-      expect(stub_comment_JIRA).to have_been_requested
+      expected_body = "{\"fields\":{\"Branch Name\":\"jt-1234-create-crawler\"}}"
+
+      fill_branch_name = stub_jira_request(:put, "/issue/jt-1234", expected_body)
+      post_comment = stub_request(
+        :post,
+        "https://example.atlassian.net/rest/api/2/issue/jt-1234/comment?startAt=0"
+      ).with(
+        body: "{\"body\":\"Opened PR: https://github.com/jobteaser/job_tomate/pull/3 - branch: jt-1234-create-crawler\"}",
+        headers: {'Authorization'=>'Basic am9iX3RvbWF0ZV91c2VybmFtZTpqb2JfdG9tYXRlX3B3ZA==', 'Content-Type'=>'application/json'}
+      ).to_return(status: 200, body: "", headers: {})
+
+      receive_stored_webhook(:github_pull_request_opened_jira_jobteaser_related)
+
+      expect(fill_branch_name).to have_been_requested
+      expect(post_comment).to have_been_requested
+    end
+
+    context "on the Career Services project" do
+      it "adds a comment on the JIRA with the PR link" do
+        expected_comment = "Opened PR: https://github.com/jobteaser/job_tomate/pull/3 - branch: cs-123-foo"
+        expected_body = "{\"body\":\"#{expected_comment}\"}"
+
+        post_comment = stub_jira_request(:post, "/issue/cs-123/comment", expected_body)
+        fill_jira_branch = stub_request(
+          :put,
+          "https://example.atlassian.net/rest/api/2/issue/cs-123?startAt=0"
+          ).with(
+            body: "{\"fields\":{\"Branch Name\":\"cs-123-foo\"}}",
+            headers: {
+              'Authorization'=>'Basic am9iX3RvbWF0ZV91c2VybmFtZTpqb2JfdG9tYXRlX3B3ZA==',
+              'Content-Type'=>'application/json'
+            }
+          ).to_return(status: 200, body: "", headers: {})
+
+        receive_stored_webhook(:github_pull_request_opened_jira_career_services_related)
+
+        expect(post_comment).to have_been_requested
+        expect(fill_jira_branch).to have_been_requested
+      end
+
+      it "sets the branch name field with the branch name" do
+        expected_body = "{\"fields\":{\"Branch Name\":\"cs-123-foo\"}}"
+
+        fill_branch_name = stub_jira_request(:put, "/issue/cs-123", expected_body)
+        post_comment = stub_request(
+          :post,
+          "https://example.atlassian.net/rest/api/2/issue/cs-123/comment?startAt=0"
+        ).with(
+          body: "{\"body\":\"Opened PR: https://github.com/jobteaser/job_tomate/pull/3 - branch: cs-123-foo\"}",
+          headers: {
+            'Authorization'=>'Basic am9iX3RvbWF0ZV91c2VybmFtZTpqb2JfdG9tYXRlX3B3ZA==',
+            'Content-Type'=>'application/json'
+          }
+        ).to_return(status: 200, body: "", headers: {})
+
+        receive_stored_webhook(:github_pull_request_opened_jira_career_services_related)
+
+        expect(fill_branch_name).to have_been_requested
+        expect(post_comment).to have_been_requested
+      end
     end
   end
 
